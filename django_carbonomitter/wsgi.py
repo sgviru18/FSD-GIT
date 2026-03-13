4aec260e37243fb9b8ef99b38a5815cab6317f27
sgviru18 <sgvirupakshagowda1199@gmail.com>
2026-03-13 19:04:05 +0530

    ready for deployment

--- django_carbonomitter/wsgi.py
+++ django_carbonomitter/wsgi.py
@@ -1,12 +1,4 @@
-"""
-WSGI config for django_carbonomitter project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_carbonomitter.settings')
